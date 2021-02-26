6bb30f2be0039603761cdf64f62dda1524fb6566
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-26 22:43:49 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -6,7 +6,7 @@ async execute(message, args) {
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
 
-      message.reply(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
+      message.channel.send(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
     } catch(e) {
       if (typeof(e) == "string") e = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
       let evalerror = new global.embed() 
