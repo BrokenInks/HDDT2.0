4b4f79486cb8960703a6210b9ed3db928f7160d8
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-26 20:38:36 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,35 +1,18 @@
-const Discord = require('discord.js');
-const { MessageEmbed } = require("discord.js");
+async execute(message, args, client) {
+        if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
+    let code = args.join(" ");
+    try {
+        let evaled = await eval(code);
+        if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 
-module.exports = {
-  name: "eval",
-  aliases: ["e"],
-  description: "Evaluated the code you puy in",
-  usage: "<code to eval>",
-  async execute(message, args, client)  {
+        message.channel.send(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
+    } catch(e) {
+        if (typeof(e) == "string") e = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
+        let evalerror = new global.embed()
+            .setTitle("Произошла ошибка")
+            .setDescription("`​``" + e + "`​``")
+            .setColor(cfg.colors.error)
+        message.channel.send(evalerror)
 
-  if(message.author.id !== '778512157926883328') return;
-  function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-      let rawEvaled = evaled;
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-        let embed = new MessageEmbed()
-      .addField(":inbox_tray: Input", `\`\`\`js\n${code}\n\`\`\``)
-      .addField(":outbox_tray: Output", `\`\`\`js\n${clean(evaled).replace(client.token, "Токен не дам)))")}\n\`\`\``)
-      .addField('Type', `\`\`\`xl\n${(typeof rawEvaled).substr(0, 1).toUpperCase() + (typeof rawEvaled).substr(1)}\n\`\`\``)
-      .setColor('GREEN');
-      message.channel.send({embed});
-    } catch (err) {
-      
-      message.channel.send(`\`Внимание, ошибка\`\` \`\`\`js\n${clean(err)}\n\`\`\``);
     }
-}
 }
