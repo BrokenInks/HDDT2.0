1c676c9a45a94bb06c77e6fbd8b4967941fc5ecd
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-26 21:00:55 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,21 +1,19 @@
-module.exports = {
-    name: 'eval',
-    category: 'owner',
-    execute(message, args, client) {
-    if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
-let code = args.join(" ");
-try {
-    let evaled = await eval(code);
-    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+async execute(message, args) {
 
-    message.reply(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
-} catch(e) {
-    if (typeof(e) == "string") e = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
-    let evalerror = new global.embed()
-        .setTitle("Произошла ошибка")
-        .setDescription("`​``" + e + "`​``")
-        .setColor(cfg.colors.error)
-    message.reply(evalerror)
+        let code = args.join(" ");
+    try {
+      let evaled = await eval(code);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
 
-}
-}
+      message.reply(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
+    } catch(e) {
+      if (typeof(e) == "string") e = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
+      let evalerror = new global.embed() 
+      .setTitle("Произошла ошибка")
+      .setDescription("`​``" + e + "`​``")
+      .setColor(cfg.colors.error)
+      message.reply(evalerror)
+      
+      }
+    }
