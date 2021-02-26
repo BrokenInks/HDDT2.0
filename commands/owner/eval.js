19965a51583fada7d5ab60226d22633c4796460d
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-26 22:52:15 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -5,11 +5,10 @@ module.exports = {
     name: 'eval',
     category: 'owner',
     execute(message, args, client) {
-        if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
-
         let code = args.join(" ");
     try {
       let evaled = await eval(code);
+      if (message.author.id !=="778512157926883328") return message.channel.send("No, this owner command");
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       message.channel.send(["Код исполнен за " + `${Date.now() - message.createdTimestamp}` + "ms\n" + evaled], { code: "js" })
     } catch(e) {
