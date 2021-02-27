29a05a8c3787fd792261b30dcb61df858d4c6b6f
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-27 16:32:30 +0300

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,6 +1,7 @@
 const { Client } = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const Discord = require('discord.js');
+const ms = require('ms');
 module.exports = {
     name: 'ping',
     cooldown: 5,
@@ -8,13 +9,13 @@ module.exports = {
     execute(message, args, client) {
         var ping = Math.round(client.ws.ping)
         const Embed = new Discord.MessageEmbed()
-                .setColor('#DAF7A6')
-                .setTitle(`Пинг: ${client.ws.ping}ms`)
-                .setURL('')
-                .setAuthor('Мой пинг', message.author.avatarURL(), 'https://discord.js.org/%27')
-                .setThumbnail(`https://cdn.discordapp.com/attachments/677202416135045130/685163406059241484/emote.png`)
-                .setTimestamp()
-                .setFooter('для '+message.author.username, message.author.avatarURL)
-                message.channel.send(Embed)
+            .setColor('#DAF7A6')
+            .setTitle(`Пинг: ${client.ws.ping}ms`)
+            .setURL('')
+            .setAuthor('Мой пинг', message.author.avatarURL(), 'https://discord.js.org/%27')
+            .setThumbnail(`https://cdn.discordapp.com/attachments/677202416135045130/685163406059241484/emote.png`)
+            .setTimestamp()
+            .setFooter('для '+message.author.username, message.author.avatarURL)
+        message.channel.send(Embed)
     },
 };
