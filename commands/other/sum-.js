498ff7a9b52f9d4beaf103ace20bf07541e82b2e
Glitch (special-brave-sign) <none>
2021-03-03 17:25:47 +0000

    🚒🔔 Updated with Glitch

--- commands/other/sum-.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'sum-',
-    category: 'other',
-    execute(message, args, client) {
-        const numArgs = args.map(x => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => counter -= x);
-    message.reply(`Сумма этих чисел будет ${sum}!`);
-    },
-};
